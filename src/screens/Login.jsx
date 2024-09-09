ffacb7d7a4f9e507cf7fa5e46fb1c34e8dc24940
Faizan-zia340 <154322813+Faizan-zia340@users.noreply.github.com>
2024-09-09 16:03:42 +0500

    update

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,14 +1,47 @@
-import React, { useState } from 'react';
+import Swal from 'sweetalert2'
+import React, {  useState } from 'react';
+import { auth } from '../database/firebase.config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+ 
 
 const Login = () => {
+    const navigate = useNavigate()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [isLoading, setisLoading] = useState(false);
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         // Handle login logic here
-        console.log('Email:', email, 'Password:', password);
-    };
+       console.log('Email:', email, 'Password:', password);
+       signInWithEmailAndPassword(auth, email, password)
+    .then((response) => {
+      const uid = response.user.uid;
+          localStorage.setItem("userId",uid)
+    
+     setisLoading(false)
+
+      Swal.fire({
+        icon: "success",
+        title: "Login Completed !",
+        text: "Do you want to continue",
+      });
+        navigate('/home')
+
+      // ...
+    })
+    .catch((error) => {   
+        console.error("Error during signup:", error);
+           Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text:error.message,
+    })
+     setisLoading(false)
+    });
+};
+    
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -44,12 +77,18 @@ const Login = () => {
                         />
                     </div>
                     <div className="flex items-center justify-between">
+                    { isLoading ?
+                    <div className="w-full flex justify-center py-2 px-4 rounded">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif" className='h-6 w-6' />
+                    </div>
+                    : 
                         <button
                             type="submit"
                             className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                         >
                             Sign In
                         </button>
+                        }
                     </div>
                 </form> 
                 <p className="text-center text-gray-600  text-sm mt-6">
@@ -62,6 +101,6 @@ const Login = () => {
             </div>
         </div>
     );
-};
 
+};
 export default Login;
--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -4,6 +4,7 @@ import { doc, setDoc } from 'firebase/firestore';
 import { db,auth } from '../database/firebase.config';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+ 
 
 
 const Signup = () => {
@@ -11,17 +12,24 @@ const Signup = () => {
     const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+   const [isLoading, setisLoading] = useState(false);
    
+
+
     const handleSignup = (e) => {
+         setisLoading(true)
         e.preventDefault();
         // Handle signup logic here
         // console.log('Full Name:', fullName, 'Email:', email, 'Password:', password);
-    };
+   
     createUserWithEmailAndPassword(auth, email, password)
     .then(async (response) => {
-      const uid = response.user;
+      const uid = response.user.uid;
       const userData= {fullName,email,password,uid}
-     await  setDoc(doc(db, "users",  uid), userData)
+      localStorage.setItem("userId",uid)
+     await  setDoc(doc(db, "users", uid), userData)
+     setisLoading(false)
+
       Swal.fire({
         icon: "success",
         title: "Signup Completed !",
@@ -38,14 +46,16 @@ const Signup = () => {
             title: "Oops...",
             text:error.message,
     })
-    
+     setisLoading(false)
     });
-  
+};
+
+   
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md bg-white p-8 shadow-lg rounded-lg">
                 <h2 className="text-2xl font-bold text-center mb-6">Signup</h2>
-                <form onSubmit={handleSignup}>
+                <form>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                        Full Name
@@ -89,6 +99,11 @@ const Signup = () => {
                         />
                     </div>
                     <div className="flex items-center justify-between">
+                    { isLoading ?
+                    <div className="w-full flex justify-center py-2 px-4 rounded">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif" className='h-6 w-6' />
+                    </div>
+                    : 
                         <button
                         onClick={handleSignup}
                             type="submit"
@@ -96,6 +111,7 @@ const Signup = () => {
                         >
                             Sign Up
                         </button>
+                        }
                     </div>
                 </form>
                 <p className="text-center text-gray-600  text-sm mt-6">
